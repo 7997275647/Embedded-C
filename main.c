63a57b41bbdd3964df8aa6413e1f60f22197280c
7997275647 <104361695+7997275647@users.noreply.github.com>
2023-03-18 13:47:22 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+
+
+{
+    struct carModel
+    {
+        uint32_t carNumber;
+        uint8_t noOfAirBags;
+        float dieselCapacity;
+    
+    };
+    struct carModel carBMW = {1234,12,45.6000};
+    struct carModel carFord = {.carNumber=1000,.noOfAirBags=10,.dieselCapacity=50.5};
+    printf("BMW Details \n");
+    printf("the car number %d\n",carBMW.carNumber);
+    printf("number of airbags %d\n",carBMW.noOfAirBags);
+    printf("the car diesel capacity %f\n",carBMW.dieselCapacity);
+    printf("Ford Details \n");
+    printf("the car number %d\n",carFord.carNumber);
+    printf("number of airbags %d\n",carFord.noOfAirBags);
+    printf("the car diesel capacity %f\n",carFord.dieselCapacity);
+    
+    
+    return 0;    
+}
