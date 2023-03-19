400be98e238c7716fc1d270d1d0b50a47ccbb27d
Sandeep Reddy Modugu <104361695+7997275647@users.noreply.github.com>
2023-03-19 14:52:57 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,29 +1,37 @@
-//-----structures in C--------
-#include <stdio.h>
-#include <stdint.h>
+#include<stdio.h>
+
+struct data
+{
+	char Data1;
+	int Data2;
+	char Data3;
+	short Data4;
+
+}varData;
 
 int main()
-
-
 {
-    struct carModel
-    {
-        uint32_t carNumber;
-        uint8_t noOfAirBags;
-        float dieselCapacity;
-    
-    };
-    struct carModel carBMW = {1234,12,45.6000};
-    struct carModel carFord = {.carNumber=1000,.noOfAirBags=10,.dieselCapacity=50.5};
-    printf("BMW Details \n");
-    printf("the car number %d\n",carBMW.carNumber);
-    printf("number of airbags %d\n",carBMW.noOfAirBags);
-    printf("the car diesel capacity %f\n",carBMW.dieselCapacity);
-    printf("Ford Details \n");
-    printf("the car number %d\n",carFord.carNumber);
-    printf("number of airbags %d\n",carFord.noOfAirBags);
-    printf("the car diesel capacity %f\n",carFord.dieselCapacity);
-    
-    
-    return 0;    
+	int Data5;
+	varData.Data1=0x11;
+	varData.Data2=0xAAAABBBB;
+	varData.Data3=0xFF;
+	varData.Data4=0xA5A5;
+	printf("Stored Value in Data1 \n");
+	printf("%x  \n",varData.Data1);
+
+	struct data *pointerData;
+
+	pointerData =&varData;
+
+	// write data
+	pointerData->Data1=0x22;
+	//read data
+	Data5=pointerData->Data1;
+	printf("Stored Value in Data1 \n");
+	printf("%x  \n",varData.Data1);
+	printf("%x  \n",Data5);
+
+
+	return 0;
 }
+
