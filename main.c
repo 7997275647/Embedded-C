a45f7bcaf679d3a692060a87aa8f57b1db3ab605
Sandeep Reddy Modugu <104361695+7997275647@users.noreply.github.com>
2023-03-19 14:55:13 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -9,29 +9,34 @@ struct data
 
 }varData;
 
+
+void displayElements(struct data varData)
+{
+	    int Data5;
+	   // write data
+	    varData.Data1=0x22;
+		//read data
+		Data5=varData.Data1;
+		printf("Stored Value in Data1 \n");
+		printf("%x  \n",varData.Data1);
+		printf("%x  \n",Data5);
+}
+
+
 int main()
 {
-	int Data5;
 	varData.Data1=0x11;
 	varData.Data2=0xAAAABBBB;
 	varData.Data3=0xFF;
 	varData.Data4=0xA5A5;
 	printf("Stored Value in Data1 \n");
 	printf("%x  \n",varData.Data1);
-
-	struct data *pointerData;
-
-	pointerData =&varData;
-
-	// write data
-	pointerData->Data1=0x22;
-	//read data
-	Data5=pointerData->Data1;
-	printf("Stored Value in Data1 \n");
-	printf("%x  \n",varData.Data1);
-	printf("%x  \n",Data5);
-
-
+	displayElements(varData);
 	return 0;
 }
+
+
+
+
+
 
